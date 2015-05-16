d7402090b7cf731867268dbe0da83986642340f9
surekhaj <surekhaj@gmail.com>
2015-05-16 15:25:41 +0530

    test commit

---
+++ apkextractor/lib/ApkParser/Config.php
@@ -0,0 +1,32 @@
+<?php
+namespace ApkParser;
+
+/**
+ * This file is part of the Apk Parser package.
+ *
+ * (c) Tufan Baris Yildirim <tufanbarisyildirim@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class Config
+{
+    private $config;
+
+    public function __construct(array $config = null)
+    {
+        if ($config == null)
+            $config = array();
+
+        $this->config = array_merge(array(
+            'tmp_path' => sys_get_temp_dir(),
+            'jar_path' => __DIR__ . '/Dex/dedexer.jar'
+        ), $config);
+    }
+
+    public function get($key)
+    {
+        return $this->config[$key];
+    }
+}
---
+++ apkextractor/lib/ApkParser/IntentFilter.php
@@ -0,0 +1,57 @@
+<?php
+namespace ApkParser;
+
+/**
+ * This file is part of the Apk Parser package.
+ *
+ * (c) Tufan Baris Yildirim <tufanbarisyildirim@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+class IntentFilter
+{
+    public $actions;
+    public $categories;
+
+
+    /**
+     * @param ManifestXmlElement $filterXml
+     */
+    public function __construct(ManifestXmlElement $filterXml)
+    {
+        $filterArray = get_object_vars($filterXml);
+
+
+        if (isset($filterArray['action'])) {
+
+            if (!is_array($filterArray['action'])) {
+                $filterArray['action'] = array($filterArray['action']);
+            }
+
+            foreach ($filterArray['action'] as $act) {
+                $actionElement = get_object_vars($act);
+                $actionNameSections = explode('.', $actionElement['@attributes']['name']);
+                $this->actions[] = end($actionNameSections);
+            }
+        }
+
+        if (isset($filterArray['category'])) {
+
+            if (!is_array($filterArray['category'])) {
+                $filterArray['category'] = array($filterArray['category']);
+            }
+
+
+            foreach ($filterArray['category'] as $cat) {
+                $categoryElement = get_object_vars($cat);
+                $categoryNameSections = explode('.', $categoryElement['@attributes']['name']);
+                $this->categories[] = end($categoryNameSections);
+            }
+        }
+
+
+    }
+
+}
